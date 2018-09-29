db05f848c165f010f31fbc154f767d0ffb428307
DarthLayer <blake.stratton@outlook.com>
2018-09-29 16:06:57 +0100

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Post
+
+posts = [
+    {
+        'author':'CoreyMS',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'August 27, 2018'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'August 28, 2018'
+    }
+]
+
+def home(request):
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
